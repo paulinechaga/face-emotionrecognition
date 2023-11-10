3be685131a7e5babcd94613021b927870d149d96
paulinechaga <paulinenyaboe0@gmail.com>
2023-11-10 17:25:11 +0300

    kk

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT =587
 EMAIL_HOST_USER ='paulinenyaboe2020@gmail.com'
-EMAIL_HOST_PASSWORD ='Starwars@2023'
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS =True
 
 SIMPLE_JWT = {
